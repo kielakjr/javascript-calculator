d2f41c8c003b544ebdba5e498b6540f4e6d747b0
mkielak <kielakowsky17@gmail.com>
2025-11-02 12:08:02 +0100

    Added buttons for calculator

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import CalculatorButton from './CalculatorButton'
+
+const calculatorButtons = [
+    'AC', "/", "X", "7", "8", "9", "-", "4", "5", "6", "+", "1", "2", "3", "=", "0", ".",
+]
 
 const Calculator = () => {
-  return (
-    <div className="w-full h-150 max-w-md p-4 bg-gray-600 rounded-lg shadow-md mx-auto mt-30">
-
-    </div>
-  )
+    return (
+        <div className="flex w-full h-120 max-w-md p-4 bg-gray-600 rounded-lg shadow-md mx-auto mt-30 flex-col items-center gap-4">
+            <div className="h-1/5 w-full bg-white rounded-md mb-4 p-2 text-right text-4xl font-mono flex items-end justify-end">
+                <span>0</span>
+            </div>
+            <div className="w-full h-2/5 mx-2 grid grid-cols-4 gap-4">
+                {calculatorButtons.map((button, index) => (
+                    <CalculatorButton key={index}>{button}</CalculatorButton>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default Calculator
--- src/components/CalculatorButton.jsx
+++ src/components/CalculatorButton.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
-const CalculatorButton = () => {
-  return (
-    <div>CalculatorButton</div>
-  )
+const CalculatorButton = ({children}) => {
+
+    let styles = "w-full h-12 font-bold rounded-lg shadow-md cursor-pointer active:scale-95 transition-transform duration-100"
+
+    if (children === "AC") {
+        styles += " bg-red-500 text-white col-span-2"
+    } else if (children === "=") {
+        styles += " bg-green-500 text-white col-start-4 row-start-4 row-end-6 h-28"
+    } else if (["/", "X", "-", "+"].includes(children)) {
+        styles += " bg-blue-500 text-white"
+    } else if (children === "0") {
+        styles += " bg-gray-300 col-span-2"
+    } else {
+        styles += " bg-gray-300 text-black"
+    }
+
+    return (
+    <button className={styles}>
+        {children}
+    </button>
+    )
 }
 
 export default CalculatorButton
