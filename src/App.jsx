b097c0b95bc2112254b4a16ae1058004cdd98eb4
mkielak <kielakowsky17@gmail.com>
2025-11-02 11:16:03 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-center font-bold underline mt-3">Hello world!</h1>
+    </>
+  )
+}
+
+export default App
