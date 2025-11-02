8db12bf99c45e4c647067a88880e92978b1debf3
mkielak <kielakowsky17@gmail.com>
2025-11-02 12:22:47 +0100

    Added ids

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -26,8 +26,8 @@ const Calculator = () => {
 
     return (
         <div className="flex w-full h-120 max-w-md p-4 bg-gray-600 rounded-lg shadow-md mx-auto mt-30 flex-col items-center gap-4">
-            <div className="h-1/5 w-full bg-white rounded-md mb-4 p-2 text-right text-4xl font-mono flex items-end justify-end">
-                <span>{input}</span>
+            <div className="h-1/5 w-full bg-white rounded-md mb-4 p-2 text-right text-4xl font-mono flex items-end justify-end tracking-wide">
+                <span className="text-black overflow-hidden" id="display">{input}</span>
             </div>
             <div className="w-full h-2/5 mx-2 grid grid-cols-4 gap-4">
                 {calculatorButtons.map((button) => (
--- src/components/CalculatorButton.jsx
+++ src/components/CalculatorButton.jsx
@@ -1,5 +1,46 @@
 import React from 'react'
 
+const getID = (buttonName) => {
+    switch (buttonName) {
+        case "AC":
+            return "clear";
+        case "/":
+            return "divide";
+        case "X":
+            return "multiply";
+        case "-":
+            return "subtract";
+        case "+":
+            return "add";
+        case "=":
+            return "equals";
+        case ".":
+            return "decimal";
+        case "0":
+            return "zero";
+        case "1":
+            return "one";
+        case "2":
+            return "two";
+        case "3":
+            return "three";
+        case "4":
+            return "four";
+        case "5":
+            return "five";
+        case "6":
+            return "six";
+        case "7":
+            return "seven";
+        case "8":
+            return "eight";
+        case "9":
+            return "nine";
+        default:
+            return null;
+    }
+}
+
 const CalculatorButton = ({children, onClick}) => {
 
     let styles = "w-full h-12 font-bold rounded-lg shadow-md cursor-pointer active:scale-95 transition-transform duration-100"
@@ -17,7 +58,7 @@ const CalculatorButton = ({children, onClick}) => {
     }
 
     return (
-    <button className={styles} onClick={onClick}>
+    <button className={styles} onClick={onClick} id={getID(children)}>
         {children}
     </button>
     )
